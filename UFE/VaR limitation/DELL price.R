b43b775f1ce747209574e29c0780e532ee7ff7b5
Лхагвасүрэн <50734813+lhagwasuren@users.noreply.github.com>
2020-02-15 10:58:30 +0800

    some graphics

--- UFE/VaR limitation/DELL price.R
+++ UFE/VaR limitation/DELL price.R
@@ -1,34 +1,58 @@
 rm(list=ls())
 
+# 1.Data and packages -----------------------------------------------------
+
 library(quantmod)
 library(dygraphs)
-library(ggplot2)
-
+library(tseries)
+library(tidyverse)
 getSymbols(Symbols = "DELL",from='2016-08-17',auto.assign = T)
 
 tail(DELL)
 str(DELL)
 
+# 2. Graphics -------------------------------------------------------------
+
 chartSeries(DELL, TA=NULL)
 barChart(DELL)
 
-
 dygraph(DELL$DELL.Close,main = 'Time series of DELL stock price'
         ,ylab = 'Currency in USD')
+
 
 P<-DELL$DELL.Close
 ra <- (P-lag(P))/lag(P)
 rg <- log(P/lag(P))
 
-ggplot(data=ra,aes(ra))+
+ra = na.omit(ra)
+rg = na.omit(rg)
+
+returns<-data.frame(ra$DELL.Close,rg$DELL.Close)
+head(returns)
+colnames(returns)<-c('ra','rg')
+
+head(returns)
+
+g_returns<-gather(returns,key='method',value = 'return')
+head(g_returns)
+
+ggplot(data=returns,aes(ra))+
   geom_histogram(bins = 18)+
   xlab('Dell')
 
-ggplot(data=ra,aes(ra))+geom_density(fill='skyblue')
+ggplot(data=g_returns,aes(x=return,color=method))+
+  geom_density()
 
+ggplot(data=g_returns,aes(x=method,y=return,fill=method))+
+  geom_boxplot()
 
-mu_a<-mean(ra,na.rm=T)
-sd_a<-sd(ra,na.rm=T)
+jarque.bera.test(ra)
+jarque.bera.test(rg)
+
+# 3. Aritmetik return -----------------------------------------------------
+
+mu_a<-mean(ra)
+sd_a<-sd(ra)
 z_a<-qnorm(.05)
 
 (VaR <- mu_a + z_a * sd_a)
@@ -36,8 +60,11 @@ z_a<-qnorm(.05)
 dygraph(ra,main='Aritmatic return and VaR',xlab = 'Date'
         ,ylab='Return') %>% dyLimit(VaR, color = "red")
 
-mu_g <- mean(rg,na.rm=T)
-sd_g <- sd(rg,na.rm=T)
+
+# 4.Geometer return ------------------------------------------------------
+
+mu_g <- mean(rg)
+sd_g <- sd(rg)
 z_g <- qnorm(.05)
 
 (VaR<-mu_g + z_g*sd_g)
